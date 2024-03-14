a05c3a15013f2909d673f55674a467d56b7ec288
cristina soriano <cristina.soris@gmail.com>
2024-03-14 15:31:52 +0100

    create map.js file #172

---
+++ staff/cris-sori/arrays/map.js
@@ -0,0 +1,31 @@
+//usando el map, crea un nuevo array, con cada uno de los numeros multiplicando por 2
+
+var numbers = [10, 20, 30, 40, 50, 60]
+
+//______________________________________
+
+//crea un nuevo array, con la longitud de cada uno de lois strings
+
+var strings = ['hola', 'mundo', 'a', 'todos']
+
+//__________________________________________
+
+//crera un nuevo array, con el resto de cada uno de los numeros
+
+var numbers2 = [12, 40, 5, 7, 24]
+
+//______________________________________
+
+//crea un nuevo array, que contenga los emails de los usuarios como string
+
+var users = [
+    { name: 'pepito', email: 'pepito@grillo.com' },
+    { name: 'wendy', email: 'wendy@darling.com' },
+    { name: 'peter', email: 'peter@pan.com' },
+    { name: 'pinocho', email: 'pin@ocho.com' },
+]
+
+//____________________________________________________
+
+//crea un nuevo array,
+// que contenga la ciudad de los usuarios como string
